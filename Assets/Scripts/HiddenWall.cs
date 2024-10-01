4f23e9cf5aaea1894900697da9905ca3ed503d3f
Lynn Wong <369150573@qq.com>
2024-10-01 15:39:22 -0400

    Level build

---
+++ Assets/Scripts/HiddenWall.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HiddenWall : MonoBehaviour
+{
+    public float hiddenTime = 1f;
+
+    private float hiddenTimer = 0f;
+    private Tilemap tilemap;
+    private float alpha = 1f;
+    private bool changing = false;
+    private bool hidding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changing)
+        {
+            hiddenTimer += Time.deltaTime;
+            if (hidding)
+            {
+                if (hiddenTimer >= hiddenTime)
+                {
+                    changing = false;
+                    tilemap.color = new Color(1, 1, 1, 0);
+                }
+                else
+                {
+                    tilemap.color = new Color(1, 1, 1, 1 - hiddenTimer/hiddenTime);
+                }
+            }
+            else
+            {
+                if (hiddenTimer >= hiddenTime)
+                {
+                    changing = false;
+                    tilemap.color = new Color(1, 1, 1, 1);
+                }
+                else
+                {
+                    tilemap.color = new Color(1, 1, 1, hiddenTimer / hiddenTime);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name + "Enter");
+        if (collision.name == "body")
+        {
+            changing = true;
+            hidding = true;
+            hiddenTimer = 0f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log(collision.name + "Exit");
+        if (collision.name == "body")
+        {
+            changing = true;
+            hidding = false;
+            hiddenTimer = 0f;
+        }
+    }
+}
